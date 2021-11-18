5669e9830d0833c6574c0782dd4f75d41c798bfb
Adrián Moreno Monterde <adrianmorenomonterde@MacBook-Pro-de-Adrian.local>
2021-11-18 02:10:09 +0100

    Avance algunas tareas

---
+++ Cafe2122/src/Aplication/App.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplication;
+
+import Conectors.ConectorDB;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author adrianmorenomonterde
+ */
+public class App 
+{
+    public static void main(String []args) throws InstantiationException, IllegalAccessException, SQLException, InterruptedException
+    {
+        String usuario = "iia2021cafe";
+        String clave = "uhuetsi2021";
+        String url = "jdbc:mysql://db4free.net:3306/iia2021";
+        ConectorDB con = new ConectorDB(); 
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try{
+           con.conexion();
+        }catch (SQLException ex){
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
--- ImplementacionCafe/src/main/java/Tareas/Distributor.java
+++ ImplementacionCafe/src/main/java/Tareas/Distributor.java
@@ -1,6 +1,16 @@
 package Tareas;
 
 import Puertos.Slot;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Distributor 
 {
@@ -8,5 +18,76 @@ public class Distributor
     private Slot entrada;
     private Slot salida1;
     private Slot salida2;
-   
+    
+    public Distributor(Slot entrada, Slot salida1, Slot salida2)
+    {
+        this.entrada = entrada;
+        this.salida1 = salida1;
+        this.salida2 = salida2;
+    }
+    
+    public Slot getEntrada()
+    {
+        return entrada;
+    }
+      
+    public Slot getSalida1()
+    {
+        return salida1;
+    }
+    
+    public Slot getSalida2()
+    {
+        return salida2;
+    }
+    
+    public void setEntrada(Slot entrada)
+    {
+        this.entrada = entrada;
+    }
+    
+    public void setSalida1(Slot salida1)
+    {
+        this.salida1 = salida1;
+    }
+    
+    public void setSalida2(Slot salida2)
+    {
+        this.salida2 = salida2;
+    }
+    
+    public void setSalida(Slot s)
+    {
+        //?????
+    }
+    
+    public void run() throws Exception
+    {
+        try
+        {
+            int contador = entrada.getCola().size();
+            
+            for(int i=0; i<contador; i++)
+            {
+                Document doc = entrada.read();
+                XPath xPath = XPathFactory.newInstance().newXPath();
+                NodeList nodo = (NodeList) xPath.compile("/bebida/tipo").evaluate(doc, XPathConstants.NODESET);
+                
+                Node beb = nodo.item(0);
+                
+                if(beb.getNodeType() == Node.ELEMENT_NODE)
+                {
+                    Element tipo = (Element) beb; 
+                    
+                    if(tipo.getTextContent().equalsIgnoreCase("caliente"))
+                        salida1.write(doc);
+                    else
+                        salida2.write(doc);
+                }
+            }
+        } catch(XPathExpressionException ex)
+        {
+            Logger.getLogger(Distributor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
